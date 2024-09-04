ef404f9ce762d7ca6863ef2ff1be6671b235d933
Jared Bruni <jaredbruni@gmail.com>
2024-09-04 09:54:34 -0700

    Web Assembly port
    ported to wasm

---
+++ wasm_port/theheader.h
@@ -0,0 +1,28 @@
+#include "mx.h"
+
+extern mxHwnd mxhwnd;
+extern mxPaint paint;
+extern mxFont font;
+extern int linenum;
+
+extern const char* getPath(const char*);
+extern void gameover();
+
+#include"mx.h"
+#include<cstring>
+#include<cstdlib>
+#include<ctime>
+#include<fstream>
+
+using namespace std;
+
+#ifndef _WIN32
+#include<unistd.h>
+#endif
+
+#define INTRO 1
+#define START 2
+#define OPTIONS 3
+#define CREDITS 4
+#define GAME 5
+#define HIGHSCORES 6
